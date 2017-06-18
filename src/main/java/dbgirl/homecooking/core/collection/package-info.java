04c308f820a7c08978dbb59276351e9c84920057
mengbo <mb9177@163.com>
2017-06-18 23:26:38 +0800

    add collection and recommendation module

---
+++ src/main/java/dbgirl/homecooking/core/collection/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *
+ * 菜谱收集
+ *
+ *
+ * Created by mengbo on 2017/6/18.
+ */
+package dbgirl.homecooking.core.collection;
---
+++ src/main/java/dbgirl/homecooking/core/recommendation/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * 菜谱推荐
+ *
+ * Created by mengbo on 2017/6/18.
+ */
+package dbgirl.homecooking.core.recommendation;
