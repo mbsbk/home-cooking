0e986f2dab2993780870acec5e10fec1d8880f7a
mengbo <mb9177@163.com>
2017-06-19 21:26:29 +0800

    add logback

--- src/test/java/dbgirl/homecooking/dao/DAOTest.java
+++ src/test/java/dbgirl/homecooking/dao/DAOTest.java
@@ -4,6 +4,8 @@ import dbgirl.homecooking.BaseTest;
 import dbgirl.homecooking.pojo.Book;
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
@@ -11,12 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class DAOTest extends BaseTest{
 
+    private final static Logger logger = LoggerFactory.getLogger(DAOTest.class);
 
     @Autowired
     private BookMapper bookMapper;
 
     @Test
     public void testConnected(){
+
+        logger.info("testing connected");
+
         Book book = bookMapper.selectByPrimaryKey("-1");
 
         Assert.assertNull(book);
